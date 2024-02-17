d8199bf16f7304d59a19e21edbfe0c1c738b820b
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-02-17 11:14:30 -0500

    Update agregarusuario.js

--- js/agregarusuario.js
+++ js/agregarusuario.js
@@ -1,8 +1,8 @@
 var nuevoUsuario = {
     id: getRandomInt(1000), 
-    correo: document.getElementById("TXT_Correo").value,
     nombres: document.getElementById("TXT_Nombres").value,
     apellidos: document.getElementById("TXT_Apellidos").value,
+    correo: document.getElementById("TXT_Correo").value,
     pass: document.getElementById("Pass_Usuario").value,
     tipouser: document.getElementById("Select_TipoUsuario").value 
 };
@@ -10,10 +10,7 @@ var nuevoUsuario = {
 
 const agregarUsuario = () => {
     console.log(nuevoUsuario);
-    document.getElementById("TXT_Correo").value ='';
-    document.getElementById("TXT_Nombres").value ='';
-    document.getElementById("TXT_Apellidos").value ='';
-    document.getElementById("Pass_Usuario").value ='';
+
 }
 
 function getRandomInt(max) {
