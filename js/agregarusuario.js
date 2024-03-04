6122100eb49dd0ba8949235962e9544271895d4e
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-04 18:39:18 -0500

    Update agregarusuario.js

--- js/agregarusuario.js
+++ js/agregarusuario.js
@@ -1,13 +1,15 @@
-const agregarUsuario = () => {
-    var nuevoUsuario = {
+var nuevoUsuario = {
         id: getRandomInt(1000), 
         nombres: document.getElementById("TXT_Nombres").value,
         apellidos: document.getElementById("TXT_Apellidos").value,
         correo: document.getElementById("TXT_Correo").value,        
         pass: document.getElementById("Pass_Usuario").value,
-        tipouser: document.getElementById("Select_TipoUsuario").value 
+        tipouser: document.getElementById("Select_TipoUsuario").value;
     };
-
+if(!(nuevoUsuario.tipouser)){
+    nuevoUsuario.tipouser = 2;
+}
+const agregarUsuario = () => {
     let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
     usuarios.push(nuevoUsuario);
