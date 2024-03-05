b7f077aac7557e6d03572226617b5c0873b8a461
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-04 20:23:57 -0500

    Update agregarusuario.js

--- js/agregarusuario.js
+++ js/agregarusuario.js
@@ -7,16 +7,11 @@ const agregarUsuario = () => {
                 pass: document.getElementById("Pass_Usuario").value,
                 tipouser: document.getElementById("Select_TipoUsuario").value
         };
-    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-    usuarios.push(nuevoUsuario);
-
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
-    
-    document.getElementById("TXT_Nombres").value = '';
-    document.getElementById("TXT_Apellidos").value = '';
-    document.getElementById("TXT_Correo").value = '';       
-    document.getElementById("Pass_Usuario").value = '';
+    if(validarPassword(nuevoUsuario.pass)){
+                manejoDatos(nuevoUsuario);
+        }else{
+                alert("La contraseña no es segura. Debe contener:\n*Minimo 1 letra mayuscula\n*Minimo 1 letra minuscula\n*Minimo 1 numero\n*Minimo 1 caracter especial\n*Minimo 8 caracteres, maximo 15")
+        } 
 }
 
 const registro = () => {
@@ -28,17 +23,35 @@ const registro = () => {
                 pass: document.getElementById("Pass_Usuario").value,
                 tipouser: 2
         };
+        if(validarPassword(nuevoUsuario.pass)){
+                manejoDatos(nuevoUsuario);
+                window.location = "../index.html";
+        }else{
+                alert("La contraseña no es segura. Debe contener:\n*Minimo 1 letra mayuscula\n*Minimo 1 letra minuscula\n*Minimo 1 numero\n*Minimo 1 caracter especial\n*Minimo 8 caracteres, maximo 15")
+        }     
+}
+
+const manejoDatos = (nuevoUsuario) => {
         let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
         
         usuarios.push(nuevoUsuario);
         
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        alert("Registro exitoso")
         
         document.getElementById("TXT_Nombres").value = '';
         document.getElementById("TXT_Apellidos").value = '';
         document.getElementById("TXT_Correo").value = '';       
         document.getElementById("Pass_Usuario").value = '';
-        window.location = "../index.html";
+}
+
+const validarPassword = (password) => {
+    const decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if(password.value.match(decimal)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const getRandomInt = (max) => {
