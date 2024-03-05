775d1a9a16ded831ccc7edd16b516fc2ac96ff85
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-04 19:54:31 -0500

    Update agregarusuario.js

--- js/agregarusuario.js
+++ js/agregarusuario.js
@@ -20,7 +20,6 @@ const agregarUsuario = () => {
 }
 
 const registro = () => {
-        alert("FUNCION ACTIVA");
         var nuevoUsuario = {
                 id: getRandomInt(1000), 
                 nombres: document.getElementById("TXT_Nombres").value,
