cc128b87e185170af729676d934afea41d96fc31
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-05 08:56:04 -0500

    Update agregarusuario.js

--- js/agregarusuario.js
+++ js/agregarusuario.js
@@ -1,23 +1,31 @@
 const agregarUsuario = () => {
     var password = document.getElementById("Pass_Usuario").value;
-    if(validarCorreo(document.getElementById("TXT_Correo").value)){
-        if(password && validarPassword(password)){
-        var nuevoUsuario = {
-            id: getRandomInt(1000), 
-            nombres: document.getElementById("TXT_Nombres").value,
-            apellidos: document.getElementById("TXT_Apellidos").value,
-            correo: document.getElementById("TXT_Correo").value,        
-            pass: password,
-            tipouser: document.getElementById("Select_TipoUsuario").value
-        };
-        manejoDatos(nuevoUsuario);
-        } else {
-            alert("La contraseña no es segura. Debe contener:\n*Minimo 1 letra mayuscula\n*Minimo 1 letra minuscula\n*Minimo 1 numero\n*Minimo 1 caracter especial\n*Minimo 8 caracteres, maximo 15");
+    
+    if(validarNombre(document.getElementById("TXT_Nombres").value)){
+        if(validarNombre(document.getElementById("TXT_Apellidos").value)){
+            if(validarCorreo(document.getElementById("TXT_Correo").value)){
+                if(password && validarPassword(password)){
+                var nuevoUsuario = {
+                    id: getRandomInt(1000), 
+                    nombres: document.getElementById("TXT_Nombres").value,
+                    apellidos: document.getElementById("TXT_Apellidos").value,
+                    correo: document.getElementById("TXT_Correo").value,        
+                    pass: password,
+                    tipouser: document.getElementById("Select_TipoUsuario").value
+                };
+                manejoDatos(nuevoUsuario);
+                } else {
+                    alert("La contraseña no es segura. Debe contener:\n*Minimo 1 letra mayuscula\n*Minimo 1 letra minuscula\n*Minimo 1 numero\n*Minimo 1 caracter especial\n*Minimo 8 caracteres, maximo 15");
+                }
+            }else{
+                alert("Correo no valido. ")
+            }  
+        }else{
+            alert("Apellido invalido");
         }
     }else{
-        alert("Correo no valido. ")
+        alert("Nombre invalido");
     }
-     
 }
 
 const registro = () => {
@@ -55,6 +63,11 @@ const manejoDatos = (nuevoUsuario) => {
 const validarCorreo = (emailInput) => {
   var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailPattern.test(emailInput);
+}
+
+const validarNombre = (nombreInput) => {
+  var nombrePattern = /^[a-zA-ZÀ-ÿ\s']+$/;
+  return nombrePattern.test(nombreInput);
 }
 
 const validarPassword = (password) => {
