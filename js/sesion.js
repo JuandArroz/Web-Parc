373c5ca4212a807b81299fd8b9353b7d03352340
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-21 15:55:58 -0500

    Update sesion.js

--- js/sesion.js
+++ js/sesion.js
@@ -20,7 +20,7 @@ const indesesion = () => {
                         <a class="nav-link text-warning" href="html/propuestas_revi.html">Propuestas</a>
                     </li>                                        
                     <li class="nav-item">
-                        <span class="nav-link text-warning"><i class="fa-solid fa-user-secret"></i> ${sesionusuario.nombres}</span>
+                        <span class="nav-link text-warning"><i class="fa-solid fa-user-secret fa-xl"></i> ${sesionusuario.nombres}</span>
                     </li>
                     <li class="nav-item">
                     <button class="btn btn-warning" id="delog" onclick="limpiar_sesion()">Cerrar sesion</button>
