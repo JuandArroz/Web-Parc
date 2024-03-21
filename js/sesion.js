d737c9fda41b1911a7e481679c4827564d0a13b3
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-21 17:35:37 -0500

    Update sesion.js

--- js/sesion.js
+++ js/sesion.js
@@ -5,7 +5,7 @@ const indesesion = () => {
 
     let opc4 = document.querySelector('#opc4');
     opc4.innerHTML += '';
-    let opc5 = document.getElementById('#opc5').value;
+    let opc5 = document.querySelector('#opc5');
     opc5.innerHTML = '';
     
     if(sesionusuario){
