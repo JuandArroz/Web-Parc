2e79ffde60b289b34ce19653997479edadf1af12
JuandArroz <128256157+JuandArroz@users.noreply.github.com>
2024-03-21 17:33:05 -0500

    Update sesion.js

--- js/sesion.js
+++ js/sesion.js
@@ -5,7 +5,7 @@ const indesesion = () => {
 
     let opc4 = document.querySelector('#opc4');
     opc4.innerHTML += '';
-    let opc5 = document.querySelector('#opc5');
+    let opc5 = document.getElementById('#opc5');
     opc5.innerHTML = '';
     
     if(sesionusuario){
@@ -21,7 +21,7 @@ const indesesion = () => {
                     <li class="nav-item">
                         <a class="nav-link text-warning" href="html/propuestas_revi.html">Propuestas</a>
                     </li>
-                `
+                `;
                 opc5.innerHtml += `
                     <li class="nav-item">
                         <span class="nav-link text-light"><i class="fa-solid fa-user-secret fa-xl"></i> ${sesionusuario.nombres}</span>
@@ -29,21 +29,23 @@ const indesesion = () => {
                     <li class="nav-item">
                         <button class="btn btn-warning" id="delog" onclick="limpiar_sesion()">Cerrar sesion</button>
                     </li>
-                `
+                `;
                 break;
         
             case 1:
                 opc4.innerHTML += `
                     <li class="nav-item">
                         <a class="nav-link text-warning" href="html/propuestas_revi.html">Propuestas</a>
-                    </li>
+                    </li>                     
+                `;
+                opc5.innerHtml += `
                     <li class="nav-item">
                         <span class="nav-link text-light"><i class="fa-solid fa-user-tie fa-xl"></i> ${sesionusuario.nombres}</span>
                     </li>
                     <li class="nav-item">
-                    <button class="btn btn-warning" id="delog" onclick="limpiar_sesion()">Cerrar sesion</button>
-                    </li>                       
-                `
+                        <button class="btn btn-warning" id="delog" onclick="limpiar_sesion()">Cerrar sesion</button>
+                    </li>  
+                `;
                 break;
         
             case 2:
