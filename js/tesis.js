0c1f0b8308ad7d3e6875eca85823c1d1fc338744
Juan Ucros <c01juandadvid@gmail.com>
2024-03-24 14:37:15 -0500

    Prueba de getAllTesis con console.log

---
+++ js/tesis.js
@@ -0,0 +1,9 @@
+const obten_data = async() =>{
+    try {
+        const response = await fetch("http://127.0.0.1:3000/getAllTesis");
+        const data = response.json();
+        console.log(data);
+    } catch (ex) {
+        alert(ex)
+    }
+}
