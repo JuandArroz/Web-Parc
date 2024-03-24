be99bb4908543343bc096193fab005137d96b138
Juan Ucros <c01juandadvid@gmail.com>
2024-03-24 14:53:05 -0500

    Prueba console.log getAllTesis N.3

--- js/tesis.js
+++ js/tesis.js
@@ -2,8 +2,13 @@ const obten_data = async() =>{
     try {
         const response = await fetch("http://127.0.0.1:3000/getAllTesis");
         const data = await response.json();
+        console.log(response);
         console.log(data);
     } catch (ex) {
         alert(ex)
     }
 }
+
+window.addEventListener("load",async()=>{
+    await obten_data();
+})
