af9694673e25cef038179b8af084993b75106954
Juan Ucros <c01juandadvid@gmail.com>
2024-03-24 15:05:53 -0500

    Funciona getAllTesis

--- js/tesis.js
+++ js/tesis.js
@@ -1,14 +1,24 @@
+var tesisData = [];
+
+const dataTable = async() =>{
+    await obten_data();
+    console.log
+    console.log(tesisData)
+    
+}
+
 const obten_data = async() =>{
     try {
         const response = await fetch("http://127.0.0.1:3000/getAllTesis");
         const data = await response.json();
-        console.log(response);
-        console.log(data);
+        
+        tesisData = data;
+
     } catch (ex) {
         alert(ex)
     }
 }
 
 window.addEventListener("load",async()=>{
-    await obten_data();
+    await dataTable();
 })
